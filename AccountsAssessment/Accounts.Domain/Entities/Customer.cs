2179b8de63950eab52471bc32d04aded0ec0612c
jlopez <jlopez@hexacta.com>
2021-11-13 20:27:36 -0300

    Add domain model entities

---
+++ AccountsAssessment/Accounts.Domain/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace Accounts.Domain.Entities
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public virtual IEnumerable<Account> Accounts { get; set; }
+    }
+}
