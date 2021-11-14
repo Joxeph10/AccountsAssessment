0b8fda621c6a6cd2a8f61d27a172aafcf870167c
jlopez <jlopez@hexacta.com>
2021-11-14 19:24:35 -0300

    AccountService UnitTests

--- AccountsAssessment/Accounts.UnitTests/DomainServices/AccountServiceTests.cs
+++ AccountsAssessment/Accounts.UnitTests/DomainServices/AccountServiceTests.cs
@@ -7,6 +7,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
+    using System.Collections.Generic;
 
     [TestClass]
     public class AccountServiceTests
@@ -55,5 +56,80 @@
         }
 
         #endregion CreateAccount
+
+        #region AddTransaction
+
+        [TestMethod]
+        public void WhenAddTransactionWithNullTransactionsShouldInvokeRepository()
+        {
+            /// Arrange
+            var account = new Account();
+            var accountTransaction = new AccountTransaction();
+
+            this._mockIRepository
+                .Setup(x => x.SaveChanges())
+                .Verifiable("SaveChanges was not Invoked");
+
+            /// Action
+            this._sut.AddTransaction(account, accountTransaction);
+
+            /// Assert 
+            this._mockIRepository.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [TestMethod]
+        public void WhenAddTransactionShouldInvokeRepository()
+        {
+            /// Arrange
+            var account = new Account { AccountTransactions = new List<AccountTransaction>() };
+            var accountTransaction = new AccountTransaction();
+
+            this._mockIRepository
+                .Setup(x => x.SaveChanges())
+                .Verifiable("SaveChanges was not Invoked");
+
+            /// Action
+            this._sut.AddTransaction(account, accountTransaction);
+
+            /// Assert 
+            this._mockIRepository.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [TestMethod]
+        public void WhenAddTransactionShouldCalculateBalance()
+        {
+            /// Arrange
+            const double ExpectedResult = 10.0;
+
+            var account = new Account
+            {
+                AccountTransactions = new List<AccountTransaction>
+                {
+                    new AccountTransaction { Id = 1, Amount = 1 },
+                    new AccountTransaction { Id = 2, Amount = 2 },
+                    new AccountTransaction { Id = 3, Amount = 3 },
+                    new AccountTransaction { Id = 4, Amount = 4 },
+                    new AccountTransaction { Id = 5, Amount = 5 }
+                }
+            };
+            var accountTransaction = new AccountTransaction
+            {
+                Id = 1,
+                Amount = -5.0,
+                TransactionType = Domain.Enumerations.ETransactionTypes.Debit
+            };
+
+            this._mockIRepository
+                .Setup(x => x.SaveChanges())
+                .Verifiable("SaveChanges was not Invoked");
+
+            /// Action
+            this._sut.AddTransaction(account, accountTransaction);
+
+            /// Assert 
+            account.Balance.Should().Be(ExpectedResult);
+        }
+
+        #endregion AddTransaction
     }
 }
